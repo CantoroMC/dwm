42b8675aeaef03ba67beddf4ce1006387cb465a2
Marco Cantoro <marco.cantoro92@outlook.it>
2021-01-06 01:34:57 +0100

    configuration tweaks

--- config.def.h
+++ config.def.h
@@ -4,8 +4,8 @@ static const unsigned int snap           = 24;           /* snap pixel */
 static const Gap default_gap             = {.isgap = 1, .realgap = 0, .gappx = 0};
 static const int showbar                 = 1;            /* 0 means no bar */
 static const int topbar                  = 1;            /* 0 means bottom bar */
-static const char buttonbar[]            = "<O>";
-static const unsigned int systraypinning = 0;            /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const char buttonbar[]            = "<Menu>";
+static const unsigned int systraypinning = 1;            /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;            /* systray spacing */
 // 1: if pinning fails, display systray on the first monitor,
 //    False: display systray on the last monitor
@@ -106,11 +106,13 @@ static const Layout layouts[] = {
 
 // Key Definitions
 static const char *layoutmenu_cmd = "xmenu_dwmlayout";
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *roficmd[]  = { "rofi", "-modi", "drun,run", "-show", "drun", NULL};
-static const char *browsercmd[]  = { "vivaldi-stable", NULL };
+static char dmenumon[2]           = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *termcmd[]      = { "st", NULL };
+static const char *browsercmd[]   = { "vivaldi-stable", NULL };
+static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *roficmd[]      = { "rofi", "-modi", "drun,run,combi", "-show", "combi", NULL};
+static const char *xmenucmd[]     = { "xmenu-apps", NULL};
+static const char *xmenuexitcmd[] = { "xmenu-shutdown", NULL};
 
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
@@ -121,6 +123,7 @@ static const char *browsercmd[]  = { "vivaldi-stable", NULL };
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define TERMINAL "st"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -139,17 +142,23 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_r,            setlayout,      {0} },
 	{ MODKEY,                       XK_t,            togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_t,            tabmode,        {-1} },
-
+	// Gaps or Spacing
 	{ MODKEY,                       XK_s,            setgaps,        {.i = +3 } },
 	{ MODKEY|ShiftMask,             XK_s,            setgaps,        {.i = -3 } },
 	{ MODKEY|ControlMask,           XK_s,            setgaps,        {.i = GAP_RESET } },
 	{ MODKEY|ShiftMask|ControlMask, XK_s,            setgaps,        {.i = GAP_TOGGLE} },
+	// Emacs and Nautilus
+	{ MODKEY,                       XK_d,            spawn,          SHCMD("emacs") },
+	{ MODKEY|ShiftMask,             XK_f,            spawn,          SHCMD("nautilus") },
 
 	{ MODKEY,                       XK_f,            spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,            togglebar,      {0} },
 
 	/*                              Right Side                                           */
 
+	// Music Players
+	{ MODKEY,                       XK_p,            spawn,          SHCMD(TERMINAL " -e ncmpcpp")},
+	{ MODKEY|ShiftMask,             XK_p,            spawn,          SHCMD(TERMINAL " -e cmus")},
 	// Focus previous/next client
 	{ MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
@@ -157,7 +166,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,            movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,            movestack,      {.i = +1 } },
 	// Resize slave clients
-	{ MODKEY|ShiftMask,             XK_l,            setsmfact,      {.f = 1.05} },
+	{ MODKEY|ShiftMask,             XK_l,            setsmfact,      {.f = -0.05} },
 	{ MODKEY|ShiftMask,             XK_h,            setsmfact,      {.f = +0.05} },
 	// Resize Master fraction
 	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
@@ -169,6 +178,7 @@ static Key keys[] = {
 	// Menu launchers
 	{ MODKEY,                       XK_u,            spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_u,            spawn,          {.v = roficmd } },
+	{ MODKEY|ControlMask,           XK_u,            spawn,          {.v = xmenucmd } },
 
 	/*                              Surrounding Keys                                     */
 
@@ -189,16 +199,28 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_bracketright, shifttag,       { .i = +1 } },
 
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return,       spawn,          SHCMD("kitty") },
+	{ MODKEY|ShiftMask,             XK_BackSpace,    spawn,          SHCMD("i3lock -i ~/.config/xmonad/screenlocker.png -t -f -e") },
+	{ MODKEY,                       XK_Delete,       spawn,          {.v = xmenuexitcmd } },
 	{ MODKEY|ShiftMask,             XK_Delete,       quit,           {0} },
 
-	{ MODKEY,                       XK_Down,         moveresize,     {.v = "0x 25y 0w 0h" } },
+	{ MODKEY,                       XK_Print,       spawn,          SHCMD("scrot ~/Pictures/Screenshots/%Y-%m-%d-%T-screenshoot.png; notify-send -i photo 'Taken fullscreen screenshot'") },
+	{ MODKEY|ShiftMask,             XK_Print,       quit,           SHCMD("import ~/Pictures/Screenshots/$(date +%Y-%m-%d-%T)screenshoot.png; notify-send -i photo 'Saved to ~/Pictures/Screenshots'") },
+
+	/*                              Arrows                                             */
+
+	{ MODKEY,                       XK_Down,         moveresize,     {.v = "0x 25y 0w 0h"  } },
 	{ MODKEY,                       XK_Up,           moveresize,     {.v = "0x -25y 0w 0h" } },
-	{ MODKEY,                       XK_Right,        moveresize,     {.v = "25x 0y 0w 0h" } },
+	{ MODKEY,                       XK_Right,        moveresize,     {.v = "25x 0y 0w 0h"  } },
 	{ MODKEY,                       XK_Left,         moveresize,     {.v = "-25x 0y 0w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Down,         moveresize,     {.v = "0x 0y 0w 25h" } },
+	{ MODKEY|ShiftMask,             XK_Down,         moveresize,     {.v = "0x 0y 0w 25h"  } },
 	{ MODKEY|ShiftMask,             XK_Up,           moveresize,     {.v = "0x 0y 0w -25h" } },
-	{ MODKEY|ShiftMask,             XK_Right,        moveresize,     {.v = "0x 0y 25w 0h" } },
+	{ MODKEY|ShiftMask,             XK_Right,        moveresize,     {.v = "0x 0y 25w 0h"  } },
 	{ MODKEY|ShiftMask,             XK_Left,         moveresize,     {.v = "0x 0y -25w 0h" } },
+	{ MODKEY|ControlMask,           XK_Down,         spawn,          SHCMD("mpc pause")},
+	{ MODKEY|ControlMask,           XK_Up,           spawn,          SHCMD("mpc play")},
+	{ MODKEY|ControlMask,           XK_Right,        spawn,          SHCMD("mpc next")},
+	{ MODKEY|ControlMask,           XK_Left,         spawn,          SHCMD("mpc prev")},
 
 	/*                              Numbers                                              */
 
@@ -215,7 +237,15 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 
 	/*                              Fn and Extra Keys                                    */
-
+	{ 0, XF86XK_AudioMute,         spawn,  SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle") },
+	{ 0, XF86XK_AudioLowerVolume,  spawn,  SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
+	{ 0, XF86XK_AudioRaiseVolume,  spawn,  SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%") },
+	{ 0, XF86XK_MonBrightnessDown, spawn,  SHCMD("xbacklight -dec 5") },
+	{ 0, XF86XK_MonBrightnessUp,   spawn,  SHCMD("xbacklight -inc 5") },
+	{ 0, XF86XK_Display,           spawn,  SHCMD("monitor_handler") },
+	{ 0, XF86XK_Search,            spawn,  SHCMD(TERMINAL " -e ranger") },
+	{ 0, XF86XK_Explorer,          spawn,  SHCMD("nautilus") },
+	{ 0, XF86XK_Calculator,        spawn,  SHCMD(TERMINAL " -e ghci") },
 };
 
 
@@ -224,23 +254,42 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkButton,            0,              Button1,        spawn,          {.v = dmenucmd } },
+	{ ClkButton,            0,              Button1,        spawn,          {.v = xmenucmd } },
+	{ ClkButton,            0,              Button3,        spawn,          {.v = xmenuexitcmd } },
+	{ ClkButton,            0,              Button2,        spawn,          SHCMD("weather") },
+	{ ClkButton,            0,              Button4,        spawn,          SHCMD("xbacklight -inc 5") },
+	{ ClkButton,            0,              Button5,        spawn,          SHCMD("xbacklight -dec 5") },
+
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-	{ ClkTagBar,            0,              Button4,        shiftview,      {.i = -1} },
-	{ ClkTagBar,            0,              Button5,        shiftview,      {.i = +1} },
+	{ ClkTagBar,            0,              Button4,        shiftview,      {.i = +1} },
+	{ ClkTagBar,            0,              Button5,        shiftview,      {.i = -1} },
+
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button2,        setlayout,      {.v = &layouts[6]} },
 	{ ClkLtSymbol,          0,              Button3,        layoutmenu,     {0} },
+	{ ClkLtSymbol,          0,              Button4,        cyclelayout,    {.i = +1 } },
+	{ ClkLtSymbol,          0,              Button5,        cyclelayout,    {.i = -1 } },
+
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkWinTitle,          0,              Button4,        movestack,      {.i = -1 } },
+	{ ClkWinTitle,          0,              Button5,        movestack,      {.i = +1 } },
+
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button3,        spawn,          SHCMD(TERMINAL " -e htop") },
+	{ ClkStatusText,        0,              Button4,        spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
+	{ ClkStatusText,        0,              Button5,        spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%") },
+
 	{ ClkRootWin,           0,              Button2,        togglebar,      {0} },
+
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+
 	{ ClkTabBar,            0,              Button1,        focuswin,       {0} },
+	{ ClkTabBar,            0,              Button2,        togglefloating, {0} },
 };
 
-// vim:ft=c:nospell
+// vim:ft=c:nospell:tw=0
