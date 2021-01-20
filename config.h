d971a59672aed2166a06b55c485a4b57efbae2be
Marco Cantoro <marco.cantoro92@outlook.it>
2021-01-20 22:09:21 +0100

    config end for now

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 // Appearance
-static const unsigned int borderpx       = 2;            /* border pixel of windows */
+static const unsigned int borderpx       = 3;            /* border pixel of windows */
 static const unsigned int snap           = 24;           /* snap pixel */
 static const Gap default_gap             = {.isgap = 1, .realgap = 0, .gappx = 0};
 static const int showbar                 = 1;            /* 0 means no bar */
 static const int topbar                  = 1;            /* 0 means bottom bar */
-static const char buttonbar[]            = "<Menu>";
+static const char buttonbar[]            = "Menu";
 static const unsigned int systraypinning = 1;            /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;            /* systray spacing */
 // 1: if pinning fails, display systray on the first monitor,
