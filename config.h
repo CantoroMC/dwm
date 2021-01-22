49dad8acbb4d7824c59c984ec45d5eea07d80948
Marco Cantoro <marco.cantoro92@outlook.it>
2021-01-22 13:29:02 +0100

    slock

--- config.h
+++ config.h
@@ -219,7 +219,7 @@ static Key keys[] = {
 
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return,       spawn,          SHCMD("kitty") },
-	{ MODKEY|ShiftMask,             XK_BackSpace,    spawn,          SHCMD("i3lock -i ~/.config/xmonad/screenlocker.png -t -f -e") },
+	{ MODKEY|ShiftMask,             XK_BackSpace,    spawn,          SHCMD("slock") },
 	{ MODKEY,                       XK_Delete,       spawn,          {.v = xmenuexitcmd } },
 	{ MODKEY|ShiftMask,             XK_Delete,       quit,           {0} },
 
