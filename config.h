7d02b1538e923e599f4bc32d5afba3fc4c0db8d2
Marco Cantoro <marco.cantoro92@outlook.it>
2021-03-25 12:23:36 +0100

    changed default terminal mappings

--- config.h
+++ config.h
@@ -219,8 +219,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_bracketright, shifttag,       { .i = +1 } },
 
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Return,       spawn,          SHCMD("tabbed -c -r 2 " TERMINAL " -w ''") },
-	{ MODKEY|ControlMask,           XK_Return,       spawn,          SHCMD("kitty") },
+	{ MODKEY|ShiftMask,             XK_Return,       spawn,          SHCMD("kitty") },
+	{ MODKEY|ControlMask,           XK_Return,       spawn,          SHCMD("tabbed -c -r 2 " TERMINAL " -w ''") },
 	{ MODKEY|ShiftMask,             XK_BackSpace,    spawn,          SHCMD("slock") },
 	{ MODKEY,                       XK_Delete,       spawn,          {.v = xmenuexitcmd } },
 	{ MODKEY|ShiftMask,             XK_Delete,       quit,           {0} },
